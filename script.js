e12afaa6866ba4b0f5d7f786c1269daed87461f0
RiteshBharvey <99973928+RiteshBharvey@users.noreply.github.com>
2022-03-10 23:24:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let randomNumber1 = Math.floor(Math.random() * 6 + 1);
-let randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+let randomImageSource1 = "dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 
 let randomNumber2 = Math.floor(Math.random() * 6 + 1);
-let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+let randomImageSource2 = "dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
